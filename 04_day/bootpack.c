1a5c949389bc074115cfe9fd26950298f9873460
marjosong <769200142@qq.com>
2025-08-25 22:20:35 +0800

    init day04

--- 04_day/bootpack.c
+++ 04_day/bootpack.c
@@ -1,17 +1,13 @@
-/* 告诉C编译器，有一个函数在别的文件里 */
-
 void io_hlt(void);
 void write_mem8(int addr, int data);
-
-/* 是函数声明却不用{}，而用;，这表示的意思是：
-	函数在别的文件中，你自己找一下 */
 
 void HariMain(void)
 {
 	int i;
 	char* p = (char*)0xa0000;
 
-	for (i = 0; i < 0xffff; i++) {
+	init_palette();
+	for (i = 0; i <= 0xffff; i++) {
 		p[i] = i & 0x0f;
 	}
 
@@ -19,4 +15,45 @@ void HariMain(void)
 		io_hlt();
 	}
 
+}
+
+void init_palette(void)
+{
+	static unsigned char table_rgb[16*3] = {
+		0x00, 0x00, 0x00,
+		0xff, 0x00, 0x00,
+		0x00, 0xff, 0x00,
+		0xff, 0xff, 0x00,
+		0x00, 0x00, 0xff,
+		0xff, 0x00, 0xff,
+		0x00, 0xff, 0xff,
+		0xff, 0xff, 0xff,
+		0xc6, 0xc6, 0xc6,
+		0x84, 0x00, 0x00,
+		0x00, 0x84, 0x00,
+		0x84, 0x84, 0x00,
+		0x00, 0x00, 0x84,
+		0x84, 0x00, 0x84,
+		0x00, 0x84, 0x84,
+		0x84, 0x84, 0x84
+	};
+
+	set_palette(0, 15, table_rgb);
+	return;
+}
+
+void set_palette(int start, int end, unsigned char* rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();
+	io_cli();
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);
+	return;
 }
